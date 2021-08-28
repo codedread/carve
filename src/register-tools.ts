267ade3d763482b8018632f5f8f8d892bff22e60
Jeff Schiller <codedread@gmail.com>
2021-08-27 23:30:45 -0700

    A very simple Single Selection tool with a crude bounding box drawn around the selected item, which you can do nothing with yet.

--- src/register-tools.ts
+++ src/register-tools.ts
@@ -3,11 +3,13 @@ import { ACTION_ELLIPSE_MODE, EllipseButton, EllipseTool } from './tools/ellipse
 import { ACTION_NEW_DOCUMENT, FileNewButton, FileNewTool } from './tools/file-new.js';
 import { ACTION_OPEN_DOCUMENT, FileOpenButton, FileOpenTool } from './tools/file-open.js';
 import { ACTION_RECTANGLE_MODE, RectangleButton, RectangleTool } from './tools/rectangle.js';
+import { ACTION_SELECT_MODE, SimpleSelectButton, SimpleSelectTool } from './tools/simple-select.js';
 
 const editor = document.querySelector('carve-editor') as CarveEditor;
 
 editor.registerTool(FileNewTool, {'carve-new-button': { ctor: FileNewButton }});
 editor.registerTool(FileOpenTool, {'carve-open-button': { ctor: FileOpenButton }})
+editor.registerTool(SimpleSelectTool, {'carve-select-button': { ctor: SimpleSelectButton }});
 editor.registerTool(RectangleTool, { 'carve-rectangle-button': { ctor: RectangleButton }});
 editor.registerTool(EllipseTool, { 'carve-ellipse-button': { ctor: EllipseButton }});
 
@@ -15,3 +17,4 @@ editor.registerKeyBinding('e', ACTION_ELLIPSE_MODE);
 editor.registerKeyBinding('n', ACTION_NEW_DOCUMENT);
 editor.registerKeyBinding('o', ACTION_OPEN_DOCUMENT);
 editor.registerKeyBinding('r', ACTION_RECTANGLE_MODE);
+editor.registerKeyBinding('s', ACTION_SELECT_MODE);
